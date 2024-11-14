de7fad59b178f4cb89154cdf45b3bec9ccc63bf2
Владимир <romanidze85@gmail.com>
2024-11-14 14:37:47 +0300

    Add files via upload

---
+++ Baza.py
@@ -0,0 +1,17 @@
+# "Базовые структуры данных"
+
+# 1st program
+print((9 ** 0.5) * 5)
+
+# 2st program
+print(9.99 > 9.98 and 1000 != 1000.1)
+
+# 3rd program
+a = 2 * 2 + 2
+b = 2 * (2 + 2)
+print(a == b)
+
+# 4th program
+x = '123.456'
+print(x[4])
+
